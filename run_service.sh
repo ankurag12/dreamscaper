c46031d419ed4fbce4fd0464a4f320492003c1f0
Ankur Agrawal <ankur.ag12@gmail.com>
2025-03-03 01:13:03 -0800

    Set DISPLAY env variable

--- run_service.sh
+++ run_service.sh
@@ -2,6 +2,7 @@
 
 # This script is used to run the service in the background
 # It is called by the AutoStart script
+export DISPLAY=:0
 cd $HOME/dreamscaper
 source ./.env/bin/activate
 python main.py >> ./stdout.log 2>&1 &
