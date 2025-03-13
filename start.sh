248d0179155166cdfbc6cba9b3f7463cc43bf52c
ankur.ag12@gmail.com <ankur.ag12@gmail.com>
2025-03-12 23:04:08 -0700

    Update start.sh to rotate logs

--- start.sh
+++ start.sh
@@ -2,8 +2,12 @@
 
 # This script is used to run the service in the background
 # It is called by the AutoStart script
-export DISPLAY=:0
 cd $HOME/dreamscaper
 source ./.env/bin/activate
 python main.py >> ./stdout.log 2>&1 &
 
+# Run a background process to trim the log every day
+while true; do
+  sleep 86400
+  tail -n 10000 stdout.log > stdout.tmp && mv stdout.tmp stdout.log
+done &
